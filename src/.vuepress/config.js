e75b4d7cdf17e850bfd238ff0f9839401ad75aa6
Bruno Paulin <brunopaulin@bpaulin.net>
2022-06-29 16:43:35 +0200

    init

---
+++ src/.vuepress/config.js
@@ -0,0 +1,61 @@
+const { description } = require('../../package')
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'Lipaulin',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  theme: '@vuepress/blog',
+  themeConfig: {
+    // Please head documentation to see the available options.
+    dateFormat: 'YYYY-MM-DD',
+    nav: [],
+    footer: {
+      contact: [
+        {
+          type: 'github',
+          link: 'https://github.com/lipaulin',
+        },
+      ],
+    },
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+    [
+      '@vuepress/blog',
+      {
+        directories: [
+          {
+            // Unique ID of current classification
+            id: 'post',
+            // Target directory
+            dirname: '_posts',
+            // Path of the `entry page` (or `list page`)
+            path: '/',
+          },
+        ],
+      },
+    ]
+  ]
+}
---
+++ src/_posts/2021-09-28-bienvenue.md
@@ -0,0 +1,7 @@
+---
+title: Bienvenue
+date: 2022-06-29
+summary: 'Je suis pas un peu jeune pour avoir un site?'
+---
+
+Mon papa m'aime très fort ... et c'est un geek.
